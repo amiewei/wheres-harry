2f150bb5b9b58cc8219489bddb34a733fa8bf968
Amie Wei <amie.wei12@gmail.com>
2023-01-18 19:25:26 -0600

    add more gameimgs

---
+++ src/components/CharBank.js
@@ -0,0 +1,292 @@
+const CharBank = [
+    {
+        id: "g1",
+        imageId: 'royal-balcony', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 569,
+            locationY: 378
+        },
+        chars: [
+            {
+                name: 'Queen Elizabeth',
+                locationX: 494,
+                locationY: 405
+            },
+            {
+                name: 'Prince William',
+                locationX: 314,
+                locationY: 371
+            },
+            {
+                name: 'Kate Middleton',
+                locationX: 352,
+                locationY: 378
+            },
+            {
+                name: 'Queen Consort Camilla',
+                locationX: 402,
+                locationY: 391
+            },
+            {
+                name: 'Meghan Markle',
+                locationX: 587,
+                locationY: 390
+            },
+        ],
+        spacingX: 10,
+        spacingY: 20,
+      },
+      {
+        id: "g2",
+        imageId: 'royal-balcony2', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 553,
+            locationY: 287
+        },
+        chars: [
+            {
+                name: 'Queen Elizabeth',
+                locationX: 489,
+                locationY: 307
+            },
+            {
+                name: 'Prince William',
+                locationX: 605,
+                locationY: 280
+            },
+            {
+                name: 'Kate Middleton',
+                locationX: 570,
+                locationY: 289
+            },
+            {
+                name: 'King George',
+                locationX: 538,
+                locationY: 295
+            },
+        ],
+        spacingX: 10,
+        spacingY: 20,
+      },
+      {
+        id: "g3",
+        imageId: 'royal-balcony3', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 565,
+            locationY: 292
+        },
+        chars: [
+            {
+                name: 'Queen Elizabeth',
+                locationX: 494,
+                locationY: 312
+            },
+            {
+                name: 'Prince William',
+                locationX: 613,
+                locationY: 288
+            },
+            {
+                name: 'King George',
+                locationX: 539,
+                locationY: 304
+            },
+            {
+                name: 'Prince Andrew',
+                locationX: 448,
+                locationY: 292
+            },
+        ],
+        spacingX: 10,
+        spacingY: 20,
+      },
+      {
+        id: "g4",
+        imageId: 'royal4', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 105,
+            locationY: 151
+        },
+        chars: [
+            {
+                name: 'Prince William',
+                locationX: 598,
+                locationY: 102
+            },
+            {
+                name: 'Meghan Markle',
+                locationX: 301,
+                locationY: 168
+            },
+            {
+                name: 'Kate Middleton',
+                locationX: 869,
+                locationY: 95
+            },
+        ],
+        spacingX: 40,
+        spacingY: 70,
+      },
+      {
+        id: "g5",
+        imageId: 'royal-family1', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 422,
+            locationY: 113
+        },
+        chars: [
+            {
+                name: 'Prince Phillip',
+                locationX: 146,
+                locationY: 176
+            },
+            {
+                name: 'Meghan Markle',
+                locationX: 892,
+                locationY: 265
+            },
+            {
+                name: 'Queen Elizabeth',
+                locationX: 513,
+                locationY: 319
+            },
+            {
+                name: 'Baby Archie',
+                locationX: 901,
+                locationY: 462
+            },
+
+        ],
+        spacingX: 90,
+        spacingY: 110,
+      },
+      {
+        id: "g6",
+        imageId: 'jubilee1', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 116,
+            locationY: 236
+        },
+        chars: [
+            {
+                name: 'Prince Phillip',
+                locationX: 805,
+                locationY: 405
+            },
+            {
+                name: 'Prince William',
+                locationX: 324,
+                locationY: 196
+            },
+            {
+                name: 'Queen Elizabeth',
+                locationX: 481,
+                locationY: 449
+            },
+            {
+                name: 'King Charles',
+                locationX: 511,
+                locationY: 255
+            },
+        ],
+        spacingX: 50,
+        spacingY: 70,
+      },
+      {
+        id: "g7",
+        imageId: 'harry-rugby-world-cup1', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 740,
+            locationY: 229
+        },
+        chars: [
+            {
+                name: 'Kate Middleton',
+                locationX: 216,
+                locationY: 432
+            },
+            {
+                name: 'Prince William',
+                locationX: 421,
+                locationY: 418
+            },
+        ],
+        spacingX: 40,
+        spacingY: 70,
+      },
+      {
+        id: "g8",
+        imageId: 'kate-wedding1', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 290,
+            locationY: 237
+        },
+        chars: [
+            {
+                name: 'Kate Middleton',
+                locationX: 506,
+                locationY: 311
+            },
+            {
+                name: 'Prince William',
+                locationX: 467,
+                locationY: 316
+            },
+            {
+                name: 'Queen Elizabeth',
+                locationX: 378,
+                locationY: 230
+            },
+            {
+                name: 'Prince Phillip',
+                locationX: 400,
+                locationY: 228
+            },
+        ],
+        spacingX: 30,
+        spacingY: 70,
+      },
+      {
+        id: "g9",
+        imageId: 'young-harry-balcony1', 
+        harry: {
+            name: 'Prince Harry',
+            locationX: 183,
+            locationY: 373
+        },
+        chars: [
+            {
+                name: 'Princess Diana',
+                locationX: 404,
+                locationY: 169
+            },
+            {
+                name: 'Prince William',
+                locationX: 371,
+                locationY: 426
+            },
+            {
+                name: 'Queen Elizabeth',
+                locationX: 714,
+                locationY: 245
+            },
+            {
+                name: 'Prince Phillip',
+                locationX: 832,
+                locationY: 151
+            },
+        ],
+        spacingX: 50,
+        spacingY: 70,
+      },
+]
+
+export default CharBank
